1194c691cc59968c1fa93e386b5e77e5c391853e
JesseYang <jesse.yang1985@gmail.com>
2018-05-15 16:53:58 +0800

    add decode part

--- cfgs/config_voc_default.py
+++ cfgs/config_voc_default.py
@@ -5,7 +5,7 @@ cfg = edict()
 cfg.base_lr = 7e-3
 cfg.end_lr = 1e-6
 
-cfg.weight_decay = 1e-4
+cfg.weight_decay = 5e-4
 
 cfg.crop_size = [513, 513]
 
@@ -28,7 +28,7 @@ cfg.num_classes = 21
 cfg.train_list = 'voc_train_sbd_aug.txt'
 cfg.test_list = 'voc_val.txt'
 
-cfg.max_itr_num = 30000
+cfg.max_itr_num = 50000
 
 cfg.momentum = 0.9
 
